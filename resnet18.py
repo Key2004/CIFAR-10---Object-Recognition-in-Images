e5f2052194b78098ba36b0bf65ab99540d9691c2
Key2004 <149941251+Key2004@users.noreply.github.com>
2024-06-04 22:35:19 +0800

    Update resnet18.py

--- resnet18.py
+++ resnet18.py
@@ -30,11 +30,8 @@ d2l.DATA_HUB['cifar10_tiny'] = (d2l.DATA_URL + 'kaggle_cifar10_tiny.zip',
                 '2068874e4b9a9f0fb07ebe0ad2b29754449ccacd')
 # 如果使用完整的Kaggle竞赛的数据集，设置demo为False
 demo = False
-# if demo:
-#     data_dir = d2l.download_extract('cifar10_tiny')
-# else:
-#     data_dir = '../data/cifar-10/'
-#@save
+
+
 def read_csv_labels(fname):
     """读取fname来给标签字典返回一个文件名"""
     with open(fname, 'r') as f:
@@ -45,11 +42,15 @@ def read_csv_labels(fname):
 labels = read_csv_labels(os.path.join(data_dir, 'trainLabels.csv'))
 print('# 训练样本 :', len(labels))
 print('# 类别 :', len(set(labels.values())))
+
+
 #@save
 def copyfile(filename, target_dir):
     """将文件复制到目标目录"""
     os.makedirs(target_dir, exist_ok=True)
     shutil.copy(filename, target_dir)
+
+
 #@save
 def reorg_train_valid(data_dir, labels, valid_ratio):
     """将验证集从原始的训练集中拆分出来"""
@@ -71,6 +72,8 @@ def reorg_train_valid(data_dir, labels, valid_ratio):
             copyfile(fname, os.path.join(data_dir, 'train_valid_test',
                 'train', label))
     return n_valid_per_label
+
+
 #@save
 def reorg_test(data_dir):
     """在预测期间整理测试集，以方便读取"""
@@ -82,60 +85,24 @@ def reorg_cifar10_data(data_dir, valid_ratio):
     labels = read_csv_labels(os.path.join(data_dir, 'trainLabels.csv'))
     reorg_train_valid(data_dir, labels, valid_ratio)
     reorg_test(data_dir)
-batch_size =  128# 32 if demo else
-valid_ratio = 0.1
-reorg_cifar10_data(data_dir, valid_ratio)
-# transform_train = torchvision.transforms.Compose([
-#     # 在高度和宽度上将图像放大到40像素的正方形
-#     torchvision.transforms.Resize(40),
-#     # 随机裁剪出一个高度和宽度均为40像素的正方形图像，
-#     # 生成一个面积为原始图像面积0.64～1倍的小正方形，
-#     # 然后将其缩放为高度和宽度均为32像素的正方形
-#     torchvision.transforms.RandomResizedCrop(32, scale=(0.64, 1.0),
-#                 ratio=(1.0, 1.0)),
-#     torchvision.transforms.RandomHorizontalFlip(),
-#     torchvision.transforms.ToTensor(),
-#     # 标准化图像的每个通道
-#     torchvision.transforms.Normalize([0.4914, 0.4822, 0.4465],
-#         [0.2023, 0.1994, 0.2010])])
-transform_train = torchvision.transforms.Compose([
-    torchvision.transforms.RandomRotation(10),
-    torchvision.transforms.Resize(128),
-    #torchvision.transforms.RandomResizedCrop(32, scale=(0.64, 1.0), ratio=(1.0, 1.0)),
-    torchvision.transforms.RandomHorizontalFlip(),
-    torchvision.transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
-    torchvision.transforms.ToTensor(),
-    torchvision.transforms.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010])
-])
 
-transform_test = torchvision.transforms.Compose([
-    torchvision.transforms.ToTensor(),
-    torchvision.transforms.Resize(128),
-    torchvision.transforms.Normalize([0.4914, 0.4822, 0.4465],
-            [0.2023, 0.1994, 0.2010])])
-train_ds, train_valid_ds = [torchvision.datasets.ImageFolder(
-    os.path.join(data_dir, 'train_valid_test', folder),
-    transform=transform_train) for folder in ['train', 'train_valid']]
-valid_ds, test_ds = [torchvision.datasets.ImageFolder(
-    os.path.join(data_dir, 'train_valid_test', folder),
-    transform=transform_test) for folder in ['valid', 'test']]
-train_iter, train_valid_iter = [torch.utils.data.DataLoader(
-    dataset, batch_size, shuffle=True, drop_last=True)
-    for dataset in (train_ds, train_valid_ds)]
-valid_iter = torch.utils.data.DataLoader(valid_ds, batch_size, shuffle=False,
-        drop_last=True)
-test_iter = torch.utils.data.DataLoader(test_ds, batch_size, shuffle=False,
-        drop_last=False)
+
+batch_size =  128 #样本数指改为128
+valid_ratio = 0.1 #训练集与测试集的比例
+reorg_cifar10_data(data_dir, valid_ratio)
+
+
 def get_net():
     num_classes = 10
     net = d2l.resnet18(num_classes, 3)
     return net
-loss = nn.CrossEntropyLoss(reduction="none")
-def train(net, train_iter, valid_iter, num_epochs, lr, wd, devices,
+
+
+def train(net, train_iter, valid_iter, num_epochs, lr, wd, devices,  #训练函数
                       lr_period, lr_decay, l1_weight, l2_weight):
     trainer = torch.optim.SGD(net.parameters(), lr=lr, momentum=0.9,
-                              weight_decay=wd)
-    scheduler = torch.optim.lr_scheduler.StepLR(trainer, lr_period, lr_decay)
+                              weight_decay=wd) #随机梯度下降
+    scheduler = torch.optim.lr_scheduler.StepLR(trainer, lr_period, lr_decay) #训练过程动态调整学习率
     num_batches, timer = len(train_iter), d2l.Timer()
     legend = ['train loss', 'train acc']
     if valid_iter is not None:
@@ -170,6 +137,35 @@ def train(net, train_iter, valid_iter, num_epochs, lr, wd, devices,
         measures += f', valid acc {valid_acc:.3f}'
     print(measures + f'\n{metric[2] * num_epochs / timer.sum():.1f}'
           f' examples/sec on {str(devices)}')
+                           
+loss = nn.CrossEntropyLoss(reduction="none")
+#数据增强部分
+transform_train = torchvision.transforms.Compose([
+    torchvision.transforms.RandomRotation(10),
+    #在高度和宽度上将图像放大到128像素的正方形
+    torchvision.transforms.Resize(128),
+    torchvision.transforms.RandomHorizontalFlip(),
+    torchvision.transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
+    torchvision.transforms.ToTensor(),
+    #标准化图像的每个通道
+    torchvision.transforms.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010])
+])
+
+transform_test = torchvision.transforms.Compose([
+    torchvision.transforms.ToTensor(),
+    torchvision.transforms.Resize(128),
+    torchvision.transforms.Normalize([0.4914, 0.4822, 0.4465],
+            [0.2023, 0.1994, 0.2010])])
+train_ds, train_valid_ds = [torchvision.datasets.ImageFolder(os.path.join(data_dir, 'train_valid_test', folder),transform=transform_train) for folder in ['train', 'train_valid']]
+valid_ds, test_ds = [torchvision.datasets.ImageFolder(os.path.join(data_dir, 'train_valid_test', folder),transform=transform_test) for folder in ['valid', 'test']]
+train_iter, train_valid_iter = [torch.utils.data.DataLoader(
+    dataset, batch_size, shuffle=True, drop_last=True)
+    for dataset in (train_ds, train_valid_ds)]
+valid_iter = torch.utils.data.DataLoader(valid_ds, batch_size, shuffle=False,
+        drop_last=True)
+test_iter = torch.utils.data.DataLoader(test_ds, batch_size, shuffle=False,
+        drop_last=False)
+
 devices, num_epochs, lr, wd = d2l.try_all_gpus(), 20, 2e-4, 5e-4
 lr_period, lr_decay, net = 4, 0.9, get_net()
 train(net, train_iter, valid_iter, num_epochs, lr, wd, devices, lr_period,
@@ -182,6 +178,7 @@ for X, _ in test_iter:
     preds.extend(y_hat.argmax(dim=1).type(torch.int32).cpu().numpy())
 sorted_ids = list(range(1, len(test_ds) + 1))
 sorted_ids.sort(key=lambda x: str(x))
+#生成csv文件提交比赛
 df = pd.DataFrame({'id': sorted_ids, 'label': preds})
 df['label'] = df['label'].apply(lambda x: train_valid_ds.classes[x])
 df.to_csv('submission.csv', index=False)
